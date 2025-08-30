a8a582f6f88428582309afd4785f8d37147d3f80
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-08-30 19:33:25 +0545

    fix

--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -23,7 +23,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME :=  lineage_rosy
+PRODUCT_NAME := lineage_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
