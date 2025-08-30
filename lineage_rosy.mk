58eecdb96497d2f58a7c867fc7a6ed5206541a23
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-08-30 19:32:24 +0545

    rosy: lineage bring-up

--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -9,27 +9,33 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Pixel customization
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Inherit some common PixelExperience stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Gapps
+-include vendor/gapps/arm64/arm64-vendor.mk
 
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_rosy
+PRODUCT_NAME :=  lineage_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Boot animation
+TARGET_BOOTANIMATION_NAME := 720
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # GApps
 TARGET_GAPPS_ARCH := arm64
--- prop.mk
+++ prop.mk
@@ -2,6 +2,9 @@
 # system.prop for rosy
 #
 
+# APEx
+ro.apex.updatable=true
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.config.media_vol_steps=25 \
@@ -311,3 +314,7 @@ wifi.interface=wlan0
 # USAP
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 persist.device_config.runtime_native.usap_pool_enabled=true
+
+# Zygote
+PRODUCT_PROPERTY_OVERRIDES += \
+zygote.critical_window.minute=10
